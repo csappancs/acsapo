669ff304899f442d1bc3a0c8f317c6b9a4359ef4
csappancs <adam.csapo@gmail.com>
2023-11-30 08:46:01 +0100

    under construction page

---
+++ src/pages/Underconstruction.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Underconstruction.css"
+
+export default function Underconstruction() {
+  return (
+    <div className='container'>
+        <h1>Under Construction</h1>
+    </div>
+  )
+}
