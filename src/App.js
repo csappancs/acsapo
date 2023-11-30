0596579789b824ddf9116637331faf9438812be2
csappancs <adam.csapo@gmail.com>
2023-11-30 19:02:38 +0100

    devre félig kész sidebar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App">
-      <div className='sidebar'>
         <Sidebar/>
-      </div>
       <div className='main-content'>
         <h4>content</h4>
       </div>
