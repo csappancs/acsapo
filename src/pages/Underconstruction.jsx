e865704512c6ef127b5b99629854d6f3c014d8ea
csappancs <adam.csapo@gmail.com>
2023-11-30 08:51:27 +0100

    commit

--- src/pages/Underconstruction.jsx
+++ src/pages/Underconstruction.jsx
@@ -3,7 +3,7 @@ import "./Underconstruction.css"
 
 export default function Underconstruction() {
   return (
-    <div className='container'>
+    <div className='underconstruction-container'>
         <h1>Under Construction</h1>
     </div>
   )
